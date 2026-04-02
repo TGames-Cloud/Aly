0444dd2d7b2028031121e1a7cc1a654d43334d18
TGames-Cloud <anthonyrodriguezm2026@gmail.com>
2026-04-02 18:47:37 -0500

    Update almacen.js

--- Sistema/almacen.js
+++ Sistema/almacen.js
@@ -2,8 +2,14 @@ const ALMACEN = {
     presupuesto: "B/. 50.00", //
     fechaRelacion: new Date(2024, 11, 3, 17, 43), //
     playlist: [
-        { id: "D6XR9GfBfwY", titulo: "Dardos" },
-        { id: "dQw4w9WgXcQ", titulo: "Never Gonna Give You Up" }
+        { id: "D6XR9GfBfwY", 
+         titulo: "Dardos",
+         bpm: 127
+        },
+        { id: "dQw4w9WgXcQ", 
+         titulo: "Never Gonna Give You Up",
+         bpm: 113
+        }
     ],
     carpetas: {
         "Recuerdos": [
