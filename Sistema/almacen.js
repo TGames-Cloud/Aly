f8e66b7c89f4ecd7d325596423edcf8cf64f57d4
TGames-Cloud <anthonyrodriguezm2026@gmail.com>
2026-04-03 18:43:17 -0500

    Update almacen.js

--- Sistema/almacen.js
+++ Sistema/almacen.js
@@ -53,46 +53,29 @@ const ALMACEN = {
     ],
     carpetas: {
         "Recuerdos": [
-            { 
+            {
                 titulo: "El inicio", 
                 fecha: "3/1/2025", 
                 nueva: false, 
-                contenido: "Yo te conocí un día como cualquier otro pero en un. Ámbito y lugar distinto a lo habitual. Ya que pues éramos unos mundos totalmente distinto tu y yo. Yo estaba en una relación y tu perseguías una.
-
-Pero en ese caso y separación yo cree algo por ti llamado interés. Interés por saber quien eras tu. Interés en saber como eras ya que conocía partes de ti y me había llamado la intensión tu manera de amar y dar cariño a la gente pero cosa que jamás yo había visto de tu parte por el simple hecho de una cosa y es que básicamente yo te conocí en el odio o maldad mutua jamás sabia o conocía en ti esa parte llamada "Amor" o "Cariño".
-
-Con el tiempo comencé a ver y observar muchas de tus actitudes qué me empezaron a dar más atención y llamaban mi atención sobre todo en tu forma de comportarte donde dependiendo de la persona siempre sacabas palabras o acciones junto a expresiones distintas. Algo muy llamativo.
-
-Con el tiempo pues fue viendo en ti algo distinto algo extraño y preocupante. Porque al momento de conocerte aunque tenias un semblante frío y controlador siempre mantenías un tipo de paciencia y alegría interna manteniendo una paz presente en ti aunque no lo notarás ni tu.
-
-Cosa que luego no logre entender hasta que aprendí más de ti" 
+                contenido: "Yo te conocí un día como cualquier otro pero en un. Ámbito y lugar distinto a lo habitual. Ya que pues éramos unos mundos totalmente distinto tu y yo. Yo estaba en una relación y tu perseguías una. Pero en ese caso y separación yo cree algo por ti llamado interés. Interés por saber quien eras tu. Interés en saber como eras ya que conocía partes de ti y me había llamado la intensión tu manera de amar y dar cariño a la gente pero cosa que jamás yo había visto de tu parte por el simple hecho de una cosa y es que básicamente yo te conocí en el odio o maldad mutua jamás sabia o conocía en ti esa parte llamada "Amor" o "Cariño". Con el tiempo comencé a ver y observar muchas de tus actitudes qué me empezaron a dar más atención y llamaban mi atención sobre todo en tu forma de comportarte donde dependiendo de la persona siempre sacabas palabras o acciones junto a expresiones distintas. Algo muy llamativo. Con el tiempo pues fue viendo en ti algo distinto algo extraño y preocupante. Porque al momento de conocerte aunque tenias un semblante frío y controlador siempre mantenías un tipo de paciencia y alegría interna manteniendo una paz presente en ti aunque no lo notarás ni tu. Cosa que luego no logre entender hasta que aprendí más de ti" 
             },
             {
                 titulo: "Acercamiento",
                 fecha: "10/1/2025",
                 nueva: false,
-                contenido: "No te conté pero lo que paso después pues fue extraño porque literalmente yo te empecé a odiar. Directamente luego de conocer muchas cosas tuyas a distancia mi ilusión y visión de ti se quebró al límite y punto donde vi tu parte más débil y suplicante donde tu sola te llevabas a modos donde no deberías de estar. Donde te empecé a odiar porque odiaba que a la persona que tanto me interese pues básicamente fuera bajada y que eliminará su semblante por un corazón qué desde lejos se notaba que no estaba interesado en nada más que no fuera más allá del cuerpo.
-
-Lamentablemente mi ilusión de una mujer luchadora y que jamás se dejaba debilitar ante nada y que siempre luchara. Pues básicamente poco a poco se me iba dejando en el olvido y que básicamente buscaba eliminar de mis intensiones. Básicamente muchas cosas vinieron a mi cabeza y tanto que buscaba solo acabar con lo más estúpido de todo hasta que se olvidara por completo de mi mente. Ya que mi persona interesante había mostrado el lado que buscaba que no tuviera.
-
-Pero no podía dejar que eso se viera por lo que tuve que mostrar un semblante agresivo y muy poco empático hacia ti por el simple hecho de que sentía rabia"
+                contenido: "No te conté pero lo que paso después pues fue extraño porque literalmente yo te empecé a odiar. Directamente luego de conocer muchas cosas tuyas a distancia mi ilusión y visión de ti se quebró al límite y punto donde vi tu parte más débil y suplicante donde tu sola te llevabas a modos donde no deberías de estar. Donde te empecé a odiar porque odiaba que a la persona que tanto me interese pues básicamente fuera bajada y que eliminará su semblante por un corazón qué desde lejos se notaba que no estaba interesado en nada más que no fuera más allá del cuerpo. Lamentablemente mi ilusión de una mujer luchadora y que jamás se dejaba debilitar ante nada y que siempre luchara. Pues básicamente poco a poco se me iba dejando en el olvido y que básicamente buscaba eliminar de mis intensiones. Básicamente muchas cosas vinieron a mi cabeza y tanto que buscaba solo acabar con lo más estúpido de todo hasta que se olvidara por completo de mi mente. Ya que mi persona interesante había mostrado el lado que buscaba que no tuviera. Pero no podía dejar que eso se viera por lo que tuve que mostrar un semblante agresivo y muy poco empático hacia ti por el simple hecho de que sentía rabia"
             },
             {
                 titulo: "Pacto entre el pasado",
                 fecha: "2/2/2025",
                 nueva: false,
-                contenido: "Aunque no lo creas seguimos juntos y talvez se vea raro pero créeme que probablemente no lo sea. Trato mucho de darte mucho pero sabes que todo esto al final tiene un límite y un final. Cosas que talvez acaben con nuestro camino. Nose hasta donde llegue esto. Nose si lleguemos a más de 6 meses, nose si a más de 8 o cuanto sea pero quiero que sepas algo. Niña yo la verdad quiero confesarte es que yo a ti como tal no te amo. Yo tengo es un valor impresionante por ti y eso se traduce en que este aquí dándote mis mejores palabras de amor. Pero como tal nosotros no estaremos juntos toda la vida.
-
-Creamos esto en el odio, en la tristeza y en mentiras. Y aunque todo se vea hermoso y se vea que funcionará aunque no lo creas probé lamente esto con el tiempo se volverá más y más inestable. Lo digo ahora porque talvez en un futuro mi mente se vuelva amante de este amor que tenemos y no pueda pensar como ahora. Pero quiero que sepas que aunque esto se acabe yo seguiré dando mi mejor esfuerzo por ser feliz. Y espero que tu del mismo modo lo hagas. Y que jamás te rindas en abrazar al cielo y recordar que vales muchísimo y que nadie deberá y podrá acabar con ese brillo que tienes tu.
-
-Niña yo tratare de dar mi mejor intento pero espero entiendas cuando todo tenga que acabarse y decir adiós porque sabes que al final yo debo continuar ya sea contigo o no. Y eso será complicado..."
+                contenido: "Aunque no lo creas seguimos juntos y talvez se vea raro pero créeme que probablemente no lo sea. Trato mucho de darte mucho pero sabes que todo esto al final tiene un límite y un final. Cosas que talvez acaben con nuestro camino. Nose hasta donde llegue esto. Nose si lleguemos a más de 6 meses, nose si a más de 8 o cuanto sea pero quiero que sepas algo. Niña yo la verdad quiero confesarte es que yo a ti como tal no te amo. Yo tengo es un valor impresionante por ti y eso se traduce en que este aquí dándote mis mejores palabras de amor. Pero como tal nosotros no estaremos juntos toda la vida. Creamos esto en el odio, en la tristeza y en mentiras. Y aunque todo se vea hermoso y se vea que funcionará aunque no lo creas probé lamente esto con el tiempo se volverá más y más inestable. Lo digo ahora porque talvez en un futuro mi mente se vuelva amante de este amor que tenemos y no pueda pensar como ahora. Pero quiero que sepas que aunque esto se acabe yo seguiré dando mi mejor esfuerzo por ser feliz. Y espero que tu del mismo modo lo hagas. Y que jamás te rindas en abrazar al cielo y recordar que vales muchísimo y que nadie deberá y podrá acabar con ese brillo que tienes tu. Niña yo tratare de dar mi mejor intento pero espero entiendas cuando todo tenga que acabarse y decir adiós porque sabes que al final yo debo continuar ya sea contigo o no. Y eso será complicado..."
             },
             {
                 titulo: "Ruptura de los lazos",
                 fecha: "10/3/2025",
                 nueva: false,
-                contenido: "Entiendo... Bueno solo me queda desearte lo mejor y espero que todo lo que vivimos no sea en vano. Se que no nos vimos pero saber lo mucho que creamos aunque fueran recuerdos pequeños sabes que valen bastante para mi y espero que también valgan para ti. Me hubiera gustado despedirme en persona y no que fuera de esta manera pero a veces muchas cosas son así. Espero logre cumplir lo que quieras pero créeme y no olvides que di me mejor intento. Sobre todo mis mejores deseos a tu futuro. Yo continuaré mi camino ahora en este colegio nuevo. No conozco a nadie pero se que lograré crear nuevos amigos y buenos recuerdos aquí
-Cuídate por favor... Sabes que te amo muchísimo... No me olvides... Te amo❤️"
+                contenido: "Entiendo... Bueno solo me queda desearte lo mejor y espero que todo lo que vivimos no sea en vano. Se que no nos vimos pero saber lo mucho que creamos aunque fueran recuerdos pequeños sabes que valen bastante para mi y espero que también valgan para ti. Me hubiera gustado despedirme en persona y no que fuera de esta manera pero a veces muchas cosas son así. Espero logre cumplir lo que quieras pero créeme y no olvides que di me mejor intento. Sobre todo mis mejores deseos a tu futuro. Yo continuaré mi camino ahora en este colegio nuevo. No conozco a nadie pero se que lograré crear nuevos amigos y buenos recuerdos aquí Cuídate por favor... Sabes que te amo muchísimo... No me olvides... Te amo❤️"
             },
             {
                 titulo: "¿De regreso a la lectura?",
