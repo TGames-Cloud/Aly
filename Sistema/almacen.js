026ec9fe166c697708cef3c28f1e18c41936d0c6
TGames-Cloud <anthonyrodriguezm2026@gmail.com>
2026-04-02 21:26:42 -0500

    Update almacen.js

--- Sistema/almacen.js
+++ Sistema/almacen.js
@@ -2,13 +2,53 @@ const ALMACEN = {
     presupuesto: "B/. 00.00", //
     fechaRelacion: new Date(2024, 11, 3, 17, 43), //
     playlist: [
-        { id: "D6XR9GfBfwY", 
-         titulo: "Dardos",
-         bpm: 127
+        { id: "8JW6qzPCkE8", 
+         titulo: "Line Without a Hook",
+         bpm: 190
         },
-        { id: "dQw4w9WgXcQ", 
-         titulo: "Never Gonna Give You Up",
-         bpm: 113
+        { id: "24u3NoPvgMw", 
+         titulo: "Heather",
+         bpm: 102
+        },
+        {
+            id: "iXjUF4BBFq4",
+            titulo: "Memories",
+            bpm: 113
+        },
+        {
+            id: "P8Jj2mp0dG8",
+            titulo: "Niño",
+            bpm: 128
+        },
+        {
+            id: "65yd5WEK0EA",
+            titulo: "The winner takes It All",
+            bpm: 126
+        },
+        {
+            id: "EuzDbYXIAXI",
+            titulo: "Her",
+            bpm: 144
+        },
+        {
+            id: "pCPYb7KPoUc",
+            titulo: "Doma",
+            bpm: 137
+        },
+        {
+            id: "cLjLcA8S2-U",
+            titulo: "Inmortal",
+            bpm: 124
+        },
+        {
+            id: "IHF57-Ia-cE",
+            titulo: "Casa en el Aire",
+            bpm: 76
+        },
+        {
+            id: "L2EmaRBEOx0",
+            titulo: "Luma",
+            bpm: 108
         }
     ],
     carpetas: {
